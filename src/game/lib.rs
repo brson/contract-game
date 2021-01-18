d0c383ca5a95f79053e2545c4ca97e53b0ee7094
Brian Anderson <andersrb@gmail.com>
2021-01-18 23:44:11 +0000

    cleanup

--- src/game/lib.rs
+++ src/game/lib.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
@@ -14,7 +15,6 @@ mod game {
     use ink_env::DefaultEnvironment;
     use ink_env::call::ExecutionInput;
     use ink_env::call::utils::ReturnType;
-    use core::convert::TryFrom;
     
     #[ink(storage)]
     pub struct Game {
@@ -43,9 +43,10 @@ mod game {
         InsufficiantBalance,
         AccountExists,
         AccountNotExists,
-        SubmitProgramFailed,
+        SubmitLevelContractFailed,
         SubmittedGreaterLevel,
-        ProgramNotExists,
+        LevelContractNotExists,
+        LevelContractCallFailed,
     }
 
     // Contract methods
@@ -69,17 +70,11 @@ mod game {
         ///
         /// - The account exists.
         /// - The paid amount is insufficient.
-        #[ink(message, payable)]
+        #[ink(message)]
         pub fn create_player_account(&mut self) -> Result<PlayerAccount, Error> {
             let caller = self.env().caller();
-            let balance = self.env().transferred_balance();
 
-            // create a captain costs 1000 money?
-            // todo: return transferred_balance on Error?
-            if balance < 1000 {
-                ink_env::debug_println("Your balance isn't enough for creating your captain");
-                Err(Error::InsufficiantBalance)
-            } else if self.have_player_account(caller) {
+            if self.have_player_account(caller) {
                 Err(Error::AccountExists)
             } else {
                 self.create_a_captain(caller)
@@ -103,15 +98,14 @@ mod game {
         /// - Level is greater than caller's current level.
         /// - Program fails verification.
         /// - Program account doesn't exist.
-        #[ink(message, payable)]
+        #[ink(message)]
         pub fn submit_level(&mut self, level: u32, level_contract: AccountId) -> Result<AccountId, Error> {
-            // todo: verify transferred balance
             let caller = self.env().caller();
             
             if let Some(player_account) = self.player_accounts.get_mut(&caller) {
                 let account_current_level = player_account.level;
                 if level <= account_current_level {
-                    player_account.level_contracts.insert(level, level_contract).ok_or(Error::SubmitProgramFailed)
+                    player_account.level_contracts.insert(level, level_contract).ok_or(Error::SubmitLevelContractFailed)
                 } else {
                     Err(Error::SubmittedGreaterLevel)
                 }
@@ -127,7 +121,7 @@ mod game {
         /// - Level is greater than caller's current level.
         /// - Caller has no submiss for this level.
         /// - Submitted program doesn't implement required contracts.
-        #[ink(message, payable)]
+        #[ink(message)]
         pub fn run_level(&mut self, level: u32) -> Result<bool, Error> {
             let caller = self.env().caller();
             if let Some(player_account) = self.player_accounts.get_mut(&caller) {
@@ -136,34 +130,16 @@ mod game {
                     ink_env::debug_println(&format!("program id: {:?}", level_contract));
                     dispatch_level(level, level_contract.clone())                    
                 } else {
-                    Err(Error::ProgramNotExists)
+                    Err(Error::LevelContractNotExists)
                 }
             } else {
                 Err(Error::AccountNotExists)
             }        
         }
 
-        #[ink(message, payable)]
-        pub fn run_level_test_2(&mut self, program_id: AccountId) -> Result<bool, Error> {
-            dispatch_level(5, program_id)
-        }
-
-        #[ink(message, payable)]
-        pub fn run_level_test(&mut self) -> bool {
-            let program_id = "4cfac7f74c6233449b5e54ba070231dd94c71b89505482cd910000656258d3ed";
-            ink_env::debug_println(&format!("hash {:?}", program_id));
-            
-            let program_id = hex::decode(program_id).unwrap();
-            ink_env::debug_println(&format!("decode {:?}", program_id));
-            
-            let program_id = AccountId::try_from(&program_id[..]).unwrap();
-            ink_env::debug_println(&format!("AccountId {:?}", program_id));
-
-            true
-        }
     }
 
-    // Methos support contracts
+    // Non-contract support methods
     impl Game {
         fn create_a_captain(&mut self, account: AccountId) -> Result<PlayerAccount, Error> {
             let new_player_account = PlayerAccount::default();
@@ -173,42 +149,50 @@ mod game {
         }
     }
 
-    fn dispatch_level(level: u32, program_id: AccountId) -> Result<bool, Error> {
-        /*let program_id = "4cfac7f74c6233449b5e54ba070231dd94c71b89505482cd910000656258d3ed";
-        ink_env::debug_println(&format!("hash {:?}", program_id));
-        
-        let program_id = hex::decode(program_id).unwrap();
-        ink_env::debug_println(&format!("decode {:?}", program_id));
-       
-        let program_id = AccountId::try_from(&program_id[..]).unwrap();
-        ink_env::debug_println(&format!("AccountId {:?}", program_id));*/
+    fn dispatch_level(level: u32, level_contract: AccountId) -> Result<bool, Error> {
 
-        ink_env::debug_println(&format!("calling flip on {:?}", program_id));
-        let return_value: () = build_call::<DefaultEnvironment>()
-            .callee(program_id) 
-            .gas_limit(50)
-            .transferred_value(10)
+        ink_env::debug_println(&format!("calling flip on {:?}", level_contract));
+
+        let return_value = build_call::<DefaultEnvironment>()
+            .callee(level_contract) 
             .exec_input(
+                // flip method
                 ExecutionInput::new(Selector::new([0xDE, 0xAD, 0xBE, 0xEF]))
             )
             .returns::<ReturnType<()>>()
-            .fire()
-            .unwrap();
+            .fire();
 
-        ink_env::debug_println(&format!("calling get on {:?}", program_id));
-        let return_value: bool = build_call::<DefaultEnvironment>()
-            .callee(program_id) 
-            .gas_limit(50)
-            .transferred_value(10)
+        match return_value {
+            Ok(_) => { },
+            Err(e) => {
+                ink_env::debug_println(&format!("flip method call failed: {:?}", e));
+                return Err(Error::LevelContractCallFailed);
+            }
+        }
+
+        ink_env::debug_println(&format!("calling get on {:?}", level_contract));
+
+        let return_value = build_call::<DefaultEnvironment>()
+            .callee(level_contract)
             .exec_input(
+                // get method
                 ExecutionInput::new(Selector::new([0xDE, 0xAD, 0xBE, 0xFF]))
             )
             .returns::<ReturnType<bool>>()
-            .fire()
-            .unwrap();
-        
-        ink_env::debug_println(&format!("return value {}", return_value));
-        Ok(return_value)
+            .fire();
+
+        match return_value {
+            Ok(heads_or_tails) => {
+                ink_env::debug_println(&format!("get method call success"));
+                ink_env::debug_println(&format!("get return value {}", heads_or_tails));
+            },
+            Err(e) => {
+                ink_env::debug_println(&format!("get method call failed: {:?}", e));
+                return Err(Error::LevelContractCallFailed);
+            }
+        }
+
+        Ok(true)
     }
     
     #[cfg(test)]
